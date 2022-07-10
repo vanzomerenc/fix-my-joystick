c3292a476c342ff8d2337a7fc22ab1a9f8e60133
Christopher van Zomeren <vanzomerenc@gmail.com>
2022-07-10 13:14:41 -0400

    Set parameters for axes on the wrapped device so the virtual device axes behave correctly.

--- jimmy.c
+++ jimmy.c
@@ -1,8 +1,10 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #include <libevdev/libevdev.h>
 #include <libevdev/libevdev-uinput.h>
 
@@ -24,6 +26,10 @@
         log("%s: " fmt " (error %d)", argv[0], ##__VA_ARGS__, last_error); \
         return last_error; }} while (0)
 
+
+static char const **add_arg(size_t *, char const ***);
+
+
 struct mapping
 {
     unsigned short type;
@@ -31,15 +37,62 @@ struct mapping
     unsigned short virt_code;
 };
 
-static char const **add_arg(size_t *, char const ***);
-
 static struct mapping *add_mapping(size_t *, struct mapping **);
 static int parse_mapping(char const *, struct mapping *);
 static void log_mapping(struct mapping const *);
 
 
-int main(int argc, char **argv)
+static void on_fatal_signal(int);
+static void global_cleanup(void);
+
+
+// For axis events (type EV_ABS), the kernel does some preprocessing
+// which changes the events sent to userspace.
+// When we create an axis on the virtual device, it goes through all
+// the same preprocessing. To avoid applying the same processing twice,
+// we need to change some parameters on the corresponding axis on the
+// real device. We need to save what these parameters were,
+// so we can restore them when we exit.
+
+struct old_absinfo
 {
+    struct libevdev *device;
+    unsigned short code;
+    struct input_absinfo info;
+};
+
+static struct old_absinfo *add_old_absinfo(size_t *, struct old_absinfo **);
+
+static size_t n_absinfo_to_restore = 0;
+static struct old_absinfo *absinfo_to_restore = NULL;
+
+
+
+
+int
+main(int argc, char **argv)
+{
+    // Disable handled signals temporarily //
+    // These signals are inconvenient to handle during initialization.
+    // We will re-enable them before starting the event loop.
+
+    sigset_t handled_signals;
+    sigset_t old_signal_mask;
+    sigemptyset(&handled_signals);
+    sigaddset(&handled_signals, SIGINT);
+    sigaddset(&handled_signals, SIGTERM);
+    sigaddset(&handled_signals, SIGHUP);
+    DIE_ON_ERROR(sigprocmask(SIG_BLOCK, &handled_signals, &old_signal_mask), "Failed to set signal mask.");
+
+
+    // Set up signal and exit handlers //
+
+    signal(SIGINT, &on_fatal_signal);
+    signal(SIGTERM, &on_fatal_signal);
+    signal(SIGHUP, &on_fatal_signal);
+    atexit(&global_cleanup);
+
+
     // Parse arguments //
 
     opterr = 0;
@@ -109,12 +162,34 @@ int main(int argc, char **argv)
         struct input_absinfo const *absinfo = libevdev_get_abs_info(real_dev, mapped[i].real_code);
         libevdev_enable_event_type(virt_dev_proto, mapped[i].type);
         libevdev_enable_event_code(virt_dev_proto, mapped[i].type, mapped[i].virt_code, absinfo);
+
+        if (mapped[i].type == EV_ABS)
+        {
+            // Store some information about this axis of the wrapped device,
+            // then modify this axis on the wrapped device so it behaves correctly on the virtual device.
+            // The stored information will be used to undo changes to the wrapped device on exit.
+
+            struct old_absinfo *restore = add_old_absinfo(&n_absinfo_to_restore, &absinfo_to_restore);
+            restore->device = real_dev;
+            restore->code = mapped[i].real_code;
+            restore->info = *absinfo;
+
+            libevdev_set_abs_fuzz(real_dev, mapped[i].real_code, 0);
+            libevdev_set_abs_flat(real_dev, mapped[i].real_code, 0);
+            struct input_absinfo const *modified_absinfo = libevdev_get_abs_info(real_dev, mapped[i].real_code);
+            libevdev_kernel_set_abs_info(real_dev, mapped[i].real_code, modified_absinfo);
+        }
     }
 
     struct libevdev_uinput *virt_dev;
     DIE_ON_ERROR(libevdev_uinput_create_from_device(
         virt_dev_proto, LIBEVDEV_UINPUT_OPEN_MANAGED, &virt_dev), "Failed to create uinput device.");
     log("Created uinput device '%s'.", libevdev_uinput_get_devnode(virt_dev));
+
+
+    // Initialization is done. It's safe to receive signals now //
+
+    DIE_ON_ERROR(sigprocmask(SIG_SETMASK, &old_signal_mask, NULL), "Failed to restore signal mask.");
 
 
     // Process events //
@@ -142,22 +217,19 @@ int main(int argc, char **argv)
 
 
 
-static char const **
-add_arg(size_t *n, char const ***args)
-{
-    *n += 1;
-    *args = reallocarray(*args, *n, sizeof(char const *));
-    return &(*args)[*n - 1];
-}
+#define ADD(n, items) \
+    *n += 1; \
+    *items = reallocarray(*items, *n, sizeof(**items)); \
+    return &(*items)[*n - 1]
 
+static char const **
+add_arg(size_t *n, char const ***args) { ADD(n, args); }
 
 static struct mapping *
-add_mapping(size_t *n, struct mapping **mapped)
-{
-    *n += 1;
-    *mapped = reallocarray(*mapped, *n, sizeof(struct mapping));
-    return &(*mapped)[*n - 1];
-}
+add_mapping(size_t *n, struct mapping **mapped) { ADD(n, mapped); }
+
+static struct old_absinfo *
+add_old_absinfo(size_t *n, struct old_absinfo **items) { ADD(n, items); }
 
 
 static int
@@ -198,4 +270,36 @@ log_mapping(struct mapping const *mapping)
         mapping->virt_code,
         libevdev_event_code_get_name(mapping->type, mapping->real_code),
         mapping->real_code);
+}
+
+
+// from "Handlers that Terminate the Process" in the GNU libc manual.
+static void
+on_fatal_signal(int sig)
+{
+    static volatile sig_atomic_t fatal_error_in_progress = 0;
+    if (fatal_error_in_progress) raise(sig);
+    fatal_error_in_progress = 1;
+
+    log("Received signal %d", sig);
+
+    global_cleanup();
+
+    signal(sig, SIG_DFL);
+    raise(sig);
+}
+
+
+static void
+global_cleanup(void)
+{
+    for (int i = 0; i < n_absinfo_to_restore; i++)
+    {
+        struct old_absinfo *restore = &absinfo_to_restore[i];
+        int last_error = libevdev_kernel_set_abs_info(
+            restore->device,
+            restore->code,
+            &restore->info);
+        if (last_error != 0) log("Failed to restore axis settings for axis %hx", restore->code);
+    }
 }
